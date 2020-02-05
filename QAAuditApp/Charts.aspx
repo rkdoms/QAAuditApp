5cd3be22daec6da93ce027b2f3622f019c066f07
Felipe Huerta <felipe.huerta@themisds.com>
2020-02-05 11:12:53 -0300

    WIP: add timer to the QA process

--- QAAuditApp/Charts.aspx
+++ QAAuditApp/Charts.aspx
@@ -1,6 +1,7 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Charts.aspx.cs" Inherits="QAAuditApp.WebForm2" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-    
+                <h2>QC GATE <b>Charts</b></h2>
+                <br />
                 <div class="section__content section__content--p30">
                     <div class="container-fluid">
                         
@@ -12,10 +13,17 @@
                                 </div>
                             </div>
                             <div class="col-lg-4">
-                                <h3 class="title-2 m-b-40">AVG Fails per Source</h3>
+                                <div class="au-card-inner">
+                                    <h3 class="title-2 m-b-40">AVG Fails per Source</h3>
+                                    <canvas id="avgFailsPerSource"></canvas>
+                                </div>
+                            </div>
+                           
+                            <div class="col-lg-3">
+                                <h3 class="title-2 m-b-40">AVG Fails</h3>
                                 <div class="statistic__item">
-                                    <h2 class="number">10,368</h2>
-                                    <h3 class="title-2 m-b-40">Avg PS </h3>
+                                    <h2 class="number avgf">0</h2>
+                                    <h3 class="title-2 m-b-20">Average Fails </h3>
                                     <div class="icon">
                                         <i class="zmdi zmdi-account-o"></i>
                                     </div>
--- QAAuditApp/Content/js/charts.js
+++ QAAuditApp/Content/js/charts.js
@@ -1,20 +1,78 @@
 ﻿(function ($) {
     // USE STRICT
     "use strict";
+    
     try {
+        $.get("Ajax/api?op=AvgFails&vals={'val1':'1','val2':'2'}", function (data) {
+            json = JSON.parse(data)
+            console.log(json.result.data[0])
+            $(".avgf").html(json.result.data[0]+"%")
+        });
 
-        //pie chart
+
+
+        //failsPerSource
         var ctx = document.getElementById("pieChart");
         var json = "";
         var source = "";
         var data = "";
         var labels = "";
-        $.get("Ajax/api?op=pieChart&vals={'val1':'1','val2':'2'}", function (data) {
+        $.get("Ajax/api?op=failsPerSource&vals={'val1':'1','val2':'2'}", function (data) {
+            console.log(data)
             json = JSON.parse(data)
+            
             if (ctx) {
 
                 ctx.height = 200;
                 var myChart = new Chart(ctx, {
+                    type: 'pie',
+                    data: {
+                        datasets: [{
+                            data: json.result.data,
+                            backgroundColor: [
+                                "rgba(0, 123, 255,0.9)",
+                                "rgba(0, 123, 255,0.7)",
+                                "rgba(0, 123, 255,0.5)",
+                                "rgba(0,0,0,0.07)"
+                            ],
+                            hoverBackgroundColor: [
+                                "rgba(0, 123, 255,0.9)",
+                                "rgba(0, 123, 255,0.7)",
+                                "rgba(0, 123, 255,0.5)",
+                                "rgba(0,0,0,0.07)"
+                            ]
+
+                        }],
+                        labels: json.result.source
+                    },
+                    options: {
+                        legend: {
+                            position: 'top',
+                            labels: {
+                                fontFamily: 'Poppins'
+                            }
+
+                        },
+                        responsive: true
+                    }
+                });
+            }
+        });
+
+
+        //AvgFailsPerSource
+        var ctx2 = document.getElementById("avgFailsPerSource");
+        var json = "";
+        var source = "";
+        var data = "";
+        var labels = "";
+        $.get("Ajax/api?op=AvgFailsPerSource&vals={'val1':'1','val2':'2'}", function (data) {
+            
+            json = JSON.parse(data)
+            if (ctx2) {
+
+                ctx2.height = 200;
+                var myChart = new Chart(ctx2, {
                     type: 'pie',
                     data: {
                         datasets: [{
@@ -54,4 +112,23 @@
     } catch (error) {
         console.log(error);
     }
+
+    /*//averageFails
+    try {
+
+        //pie chart
+        var ctx = document.getElementById("pieChart2");
+        var json = "";
+        var source = "";
+        var data = "";
+        var labels = "";
+        $.get("Ajax/api?op=averageFails&vals={'val1':'1','val2':'2'}", function (data) {
+            $(".avgf").html(data)
+        });
+
+
+
+    } catch (error) {
+        console.log(error);
+    }*/
 })(jQuery);
